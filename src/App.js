d30f63ba6de54131ea2dc54b60997ebfd3fae5d7
rubiocode <w.rubidia.rubio@gmail.com>
2022-03-03 15:50:16 -0800

    create folders and intall npm packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle } from "./globalStyles";
+import { lazy, Suspense } from "react";
 
-function App() {
+const Home = lazy(() => import("./pages/Home"));
+const Header = lazy(() => import("./components/Header/Header"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const ScrollToTop = lazy(() => import("./components/ScrollToTop/ScrollToTop"));
+
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Suspense fallback={null}>
+        <GlobalStyle />
+        {/* <h1>Hello World</h1> */}
+        <Header />
+        <Home />
+        <Footer />
+        <ScrollToTop />
+      </Suspense>
+    </>
   );
 }
-
-export default App;

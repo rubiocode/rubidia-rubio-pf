7dd9dcb8fdcda0cfd9411076c30369bbb7639d16
rubiocode <w.rubidia.rubio@gmail.com>
2022-04-18 21:36:16 -0700

    figma vector for resume point created

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -38,7 +38,7 @@ const About = () => {
         <Section>
             <Container>
                 <Box>
-                    About Me // 
+                    About Me // modified add image 
                 </Box>
             </Container>
         </Section>
